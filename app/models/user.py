88b6c604b18daeda59a2cea514559bd4512e3cee
Nabawi <nabawicode@gmail.com>
2026-07-11 14:42:41 +0700

    feat: create user model and init table

---
+++ app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(100))
+    password: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
